5baa84458271922216a1efc32968e9f0096278f7
Brandon Henry <bhenry@campaignmonitor.com>
2020-05-05 01:09:09 -0400

    happy birthday coco

---
+++ coco/js/main.js
@@ -0,0 +1,29 @@
+//Carousel code
+
+const carousel = document.getElementById("carousel");
+const images = carousel.children;
+var cur = 0;
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+function hideImage(i) {
+  images[i].style.display = "none";
+}
+
+function showImage(i) {
+  images[i].style.display = "block";
+}
+
+function swapImage() {
+  let i = getRandomInt(0, images.length);
+  hideImage(cur);
+  cur = i;
+  showImage(cur);
+}
+
+swapImage();
+setInterval(swapImage, 2000);
